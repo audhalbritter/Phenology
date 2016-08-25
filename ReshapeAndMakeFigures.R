f3cf4b845b59bdfca69e1f05c23ec83642538d60
audhalbritter <me@audhalbritter.com>
2016-08-25 12:16:15 +0200

    Rename file: MakeFigures

---
+++ ReshapeAndMakeFigures.R
@@ -0,0 +1,35 @@
+# Phenology data 2015
+
+#### LIBRARIES ####
+library("lme4")
+library("tidyr")
+library("dplyr")
+library("lubridate")
+library("ggplot2")
+
+#### IMPORT PHENO.LONG ####
+load("PhenoLong.RData", verbose = TRUE)
+
+
+#### RESHAPE DATA TO MAKE PLOTS ####
+# destination
+pheno.treat.dest <- ReshapeToMakeFigure(pheno.long, "destination", "C1")
+head(pheno.treat.dest)
+
+# origin
+pheno.treat.orig <- ReshapeToMakeFigure(pheno.long, "origin", "C1")
+head(pheno.treat.orig)
+
+
+
+library("cowplot")
+MakePlot2(pheno.treat.dest, "destination", "first", "f", "dCumTemp", "DOY")
+MakePlot2(pheno.treat.orig, "origin", "first", "f", "oCumTemp", "DOY")
+
+
+PhenologyPlot1 <- 
+# + theme_grey(base_size = 24)+theme(legend.position= "top")
+
+
+### Plot for functionalGroup
+MakePlotFuncGroup(pheno.treat.orig, "origin", "duration", "f", "doy", "DOY")
