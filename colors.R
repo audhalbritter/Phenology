e90e1285d1f1115acdaed598b3768bc50018e6b3
audhalbritter <me@audhalbritter.com>
2016-09-27 08:12:54 +0200

    Define color blind colors

---
+++ colors.R
@@ -0,0 +1,9 @@
+#### COLORS #####
+
+col.orange <- "#E69F00"
+col.lblue <- "#56B4E9"
+col.green <- "#009E73"
+col.yellow <- "#F0E442"
+col.dblue <- "#0072B2"
+col.red <- "#D55E00"
+col.pink <- "#CC79A7"
