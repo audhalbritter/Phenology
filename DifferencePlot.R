5dc0c7a201dae04c1a4231e5540d356c9ca32c2b
audhalbritter <me@audhalbritter.com>
2017-06-30 13:31:56 +0200

    Select all species, with control and at least one treatment from the same site; can calculate diff in treatment - control
    Test this for one species

--- DifferencePlot.R
+++ DifferencePlot.R
@@ -1,5 +1,7 @@
 # DIFFERENCE PLOTS FOR 7 COMMON SPECIES
 
+library("cowplot")
+
 #### Common species ####
 # Find species that occur in Control and at least one other treatment, and more than 2 sites  
 dd <- Phenology %>% 
@@ -7,36 +9,73 @@ dd <- Phenology %>%
   select(siteID, species, newTT, value) %>%
   group_by(siteID, species, newTT) %>% 
   summarise(mean = mean(value)) %>% 
-  spread(key = newTT, value = mean) %>% 
-  mutate(TooFew = ifelse(is.na(Warm) & is.na(Wet) & is.na(WarmWet), "few", "enough")) %>% 
+  spread(key = newTT, value = mean) %>%
+  filter(!is.na(Control)) %>% 
+  mutate(TooFew = ifelse(is.na(Warmer) & is.na(LaterSM) & is.na(WarmLate), "few", "enough")) %>% 
   filter(TooFew != "few") %>% 
-  select(-TooFew) %>%
-  filter(!is.na(Control))
+  select(-TooFew) %>% 
+  gather(key = Treatment, value = value, -siteID, -species)
 
-ddd <- data.frame(table(dd$species, dd$siteID))
-colnames(ddd) <- c("species","siteID", "Frequency")
-sp.list <- ddd %>% 
-  spread(key = siteID, value = Frequency) %>% 
-  mutate(sum = rowSums(.[2:7])) %>% 
-  filter(sum > 2) %>% 
-  distinct(species)
+sp.list <- sort(unique(dd$species))
 
+
+# SMDiff by siteID and treatments
+EventDiffData %>% 
+  #filter(newTT != "Control") %>% 
+  ggplot(aes(x = SMDiff, y = value, color = newTT, shape = siteID)) +
+  geom_vline(xintercept = 0, color = "grey", linetype = "dashed") +
+  geom_jitter() + 
+  scale_colour_manual(name = "Treatment:", values = c("grey", "red", "blue", "purple")) +
+  facet_wrap(~ species)
+
+  
 
 #### Difference plots ####
-### Select 7 species, calc diff. control-treatment,plot
+### Select species, pheno.unit, etc.
 EventDiffData <- Phenology %>% 
-  filter(species %in% c("Alc.alp", "Ant.odo", "Bis.viv", "Luz.mul", "Nar.str", "Pin.vul", "Pot.ere")) %>% 
+  filter(species %in% sp.list) %>% 
   filter(siteID != "Veskre" | newTT != "Control") %>% # remove Control at Veskre
-  filter(pheno.unit == "DaysSinceSM", pheno.stage == "Flower", pheno.var == "first") %>% 
-  #mutate(orig = plyr::mapvalues(orig, c("GUD", "RAM", "SKJ"), c("Alpine-early", "Subalpine-early", "Alpine-late"))) %>%
-  #mutate(orig = factor(orig, levels = c("Alpine-early", "Alpine-late", "Subalpine-early"))) %>%
-  group_by(newTT, destSiteID, siteID, species) %>% 
-  summarise(N = n(), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
-  filter(species == "Pot.ere")
+  # remove pheno.stage and .var later
+  filter(pheno.unit == "DaysSinceSM", pheno.stage == "Flower", pheno.var == "first")
 
+# Calculate SMDifference within siteID
+SMD <- EventDiffData %>% 
+  filter(newTT != "Control") %>% 
+  group_by(newTT, siteID, species) %>% 
+  summarise(SMDiff = mean(SMDiff, na.rm = TRUE))
+
+MetaData <- turfs.15 %>% 
+  select(siteID, blockID, newTT, o.wsm15.wnr, d.wsm15.wnr) %>% 
+  mutate(SMDiff = d.wsm15.wnr - o.wsm15.wnr) %>% 
+  mutate(Treatment = plyr::mapvalues(newTT, c("control", "TT2", "TT3", "TT4"), c("Control", "Warmer", "LaterSM", "WarmLate"))) %>%
+  left_join(ClimateContext, by = c("siteID", "Treatment"))
+
+EventDiffData %>% 
+  select(blockID, species, value, siteID, newTT, SMDiff) %>% 
+  filter(species == "Ant.odo") %>% 
+  group_by(newTT, blockID) %>% 
+  # mean value per treatment, origin and destination site and species
+  summarise(mean = mean(value, na.rm = TRUE)) %>% 
+  spread(key = newTT, value = mean) %>% 
+  filter(!is.na(Control)) %>% 
+  mutate(Warmer = Warmer - Control, LaterSM = LaterSM - Control, WarmLate = WarmLate - Control) %>%
+  select(-Control) %>% 
+  gather(key = Treatment, value = value, -blockID) %>% 
+  filter(!is.na(value)) %>% 
+  left_join(MetaData, by = c("blockID", "newTT")) %>% 
+  ggplot(aes(x = SMDiff, y = value, color = Treatment, shape = Shape, alpha = Alpha)) +
+  geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
+  geom_point() +
+  scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
+  scale_shape_manual(name = "Climate Context:", values = c(1, 16, 16, 17, 17, 15, 15)) +
+  scale_alpha_manual(name = "Climate Context:", values = c(1, 0.5, 1, 0.5, 1, 0.5, 1))
+
+# calc diff. treatment - control
 EventDiff <- EventDiffData %>% 
-  ungroup(destSiteID) %>% 
-  select(-destSiteID,-N) %>% 
+  group_by(newTT, siteID, species) %>% 
+  # mean value per treatment, origin and destination site and species
+  summarise(N = n(), mean = mean(value, na.rm = TRUE), se = sd(value, na.rm = TRUE)/sqrt(N)) %>% 
+  select(-N) %>% 
   unite(united, mean, se, sep = "_") %>%
   spread(key = newTT, value = united) %>% 
   separate(col = Control, into = c("Control_mean", "Control_se"), sep = "_", convert = TRUE) %>% 
@@ -53,22 +92,14 @@ EventDiff <- EventDiffData %>%
   unite(WarmLate, WarmLate_mean, WarmLate_se, sep = "_") %>% 
   gather(key = Treatment, value = united, -siteID, -species) %>% 
   separate(col = united, into = c("mean", "se"), sep = "_", convert = TRUE) %>% 
-  #left_join(xAxis1, by = c("Treatment", "orig")) %>% 
   filter(!is.na(mean))
-
-SMD <- Phenology %>% 
-  filter(species %in% c("Alc.alp", "Ant.odo", "Bis.viv", "Luz.mul", "Nar.str", "Pin.vul", "Pot.ere")) %>% 
-  filter(siteID != "Veskre" | newTT != "Control") %>% # remove Control at Veskre
-  filter(newTT != "Control") %>% 
-  filter(pheno.unit == "DaysSinceSM", pheno.stage == "Flower", pheno.var == "first") %>% 
-  group_by(newTT, siteID, species) %>% 
-  summarise(SMDiff = mean(SMDiff, na.rm = TRUE))
 
 
 ClimateContext <- data_frame(
   siteID = c("Lavisdalen", "Gudmedalen", "Skjellingahaugen", "Lavisdalen", "Gudmedalen", "Hogsete", "Rambera", "Lavisdalen", "Gudmedalen"),
   Treatment = c(rep("Warmer", 3), rep("LaterSM", 4), rep("WarmLate", 2)),
-  Shape = c("dry", "intermediate", "wet", "alpine", "alpine", "subalpine", "subalpine", "dry", "intermediate")
+  Shape = c("dry", "intermediate", "wet", "alpine-intermediate", "alpine-wet", "subalpine-intermediate", "subalpine-wet", "dry", "intermediate"),
+  Alpha = c("dry", "intermediate", "wet", "alpine-intermediate", "alpine-wet", "subalpine-intermediate", "subalpine-wet", "dry", "intermediate")
 )
 
 
@@ -77,13 +108,18 @@ EventDiff %>%
   left_join(ClimateContext, by = c("siteID", "Treatment")) %>% 
   mutate(Treatment = plyr::mapvalues(Treatment, c("Warmer", "LaterSM", "WarmLate"), c("Warmer", "Later SM", "Warm & late SM"))) %>%
   mutate(Treatment = factor(Treatment, levels = c("Warmer", "Later SM", "Warm & late SM"))) %>%
-  mutate(Shape = factor(Shape, levels = c("dry", "intermediate", "wet", "alpine", "subalpine"))) %>%
-  ggplot(aes(x = SMDiff, y = mean, color = Treatment, shape = Shape)) +
+  mutate(Shape = factor(Shape, levels = c("dry", "intermediate", "wet", "alpine-intermediate", "alpine-wet", "subalpine-intermediate", "subalpine-wet"))) %>%
+  mutate(Alpha = factor(Alpha, levels = c("dry", "intermediate", "wet", "alpine-intermediate", "alpine-wet", "subalpine-intermediate", "subalpine-wet"))) %>%
+  ggplot(aes(x = SMDiff, y = mean, 
+             ymin = mean - se, ymax = mean + se,
+             color = Treatment, shape = Shape, alpha = Alpha)) +
   geom_hline(yintercept = 0, color = "grey", linetype = "dashed") +
-  geom_point(size = 2) +
+  geom_errorbar() +
+  geom_jitter(size = 3) +
   scale_colour_manual(name = "Treatment:", values = c("red", "blue", "purple")) +
-  scale_shape_manual(name = "Climate Context:", values = c(1, 18, 16, 17, 15)) +
-  labs(y = "Difference in onset of flowering [days] after SMT \n between treatment and origin-control", x = "Difference in SMT between origin and destination site [weeks]") +
+  scale_shape_manual(name = "Climate Context:", values = c(1, 16, 16, 17, 17, 15, 15)) +
+  scale_alpha_manual(name = "Climate Context:", values = c(1, 0.5, 1, 0.5, 1, 0.5, 1)) +
+  labs(y = "Difference in onset of flowering [days] after SMT \n between treatment and origin-control", x = "Difference in SMT between destination and origin site [weeks]") +
   facet_wrap(~ species)
 
 
