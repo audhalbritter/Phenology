d8f1a9eda335eda9e7ca9167170cf7c4e36c39be
audhalbritter <audhalbritter.com>
2017-12-06 08:29:34 +0100

    move old code

---
+++ Old Figures/Presentation_Ecosummit.R
@@ -0,0 +1,13 @@
+#### ANALYSE DATA ####
+
+#### LIBRARIES ####
+library("lme4")
+library("tidyr")
+library("dplyr")
+library("lubridate")
+library("ggplot2")
+library("cowplot")
+
+#### IMPORT PHENO.LONG ####
+load("PhenoLong.RData", verbose = TRUE)
+
